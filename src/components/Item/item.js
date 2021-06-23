495f8690155563af3294be97c12f80ceb209cc2e
Mariana Hiba <mariana_hiba@hotmail.com>
2021-06-23 15:34:14 -0300

    mejoras a la entrega desafio 7

--- src/components/Item/item.js
+++ src/components/Item/item.js
@@ -1,17 +1,12 @@
-import ItemCount from '../ItemCount/ItemCount';
 
 export const Item = ({ product }) => {
-
-    const onAdd = (cantCart) => {
-        alert(`Se agregaron al carrito ${cantCart} items`);
-    };
 
     return (
       <div className="productWrapper">
         <img src={product.img} alt="product" />
         <p>{product.title}</p>
         <p>${product.price}</p>
-        <ItemCount stock = { product.stock } valorInicial = { 1 } onAdd = { onAdd } />
+        
       </div>
        
     )
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,10 +1,18 @@
+import ItemCount from '../ItemCount/ItemCount';
+
 export const ItemDetail = ({ product }) => {
+    
+    const onAdd = (cantCart) => {
+        alert(`Se agregaron al carrito ${cantCart} items`);
+    };
 
     return ( 
-        <div>
+        <div className="detalleItem">
             <img src = { product.img } alt = "product" />
             <p>{product.title}</p>
             <p>${product.price}</p>
+            <p>Stock: {product.stock}</p>
+            <ItemCount stock = { product.stock } valorInicial = { 1 } onAdd = { onAdd } />
         </div>
     );
 };
