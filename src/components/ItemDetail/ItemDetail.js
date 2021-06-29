6b563074a88679d00375d304e8e4f4155b5a2451
Mariana Hiba <mariana_hiba@hotmail.com>
2021-06-29 16:55:26 -0300

    entrega desafio 9

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,13 +1,22 @@
+import { useEffect, useState } from "react";
 import ItemCount from '../ItemCount/ItemCount';
 import {Link} from "react-router-dom";
 import './ItemDetail.css'
 
 export const ItemDetail = ({ product }) => {
-    
-    const onAdd = (cantCart) => {
-        alert(`Se agregaron al carrito ${cantCart} items`);
+    const [cantCart,setCantCart] = useState(0);
+    const [btnPayment,setBtnPayment] = useState(false);
+
+    const onAdd = (cantidad) => {
+        setCantCart(cantidad);
     };
     
+    useEffect(() => {
+        if (cantCart > 0) {
+            setBtnPayment(true);
+        }
+    }, [cantCart]);
+
     return ( 
         <div className="detalleItem">
             <img src = { product.img } alt = "producto" />
@@ -15,6 +24,10 @@ export const ItemDetail = ({ product }) => {
             <p>${product.price}</p>
             <p>Stock: {product.stock}</p>
             <ItemCount stock = { product.stock } valorInicial = { 1 } onAdd = { onAdd } />
+            {btnPayment ? (<Link to="/Cart" className="btn-payment" onClick={() => {
+                setCantCart(0);}}>Terminar mi compra</Link>
+            ) : null}
+            <br></br>
             <Link to="/">Volver al Inicio</Link>
         </div>
     );
